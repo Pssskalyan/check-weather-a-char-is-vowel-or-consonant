f018bc54412c8d745436463de9c2313dee1038d0
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-05-07 16:22:21 +0530

    Create pavan.py

---
+++ pavan.py
@@ -0,0 +1,9 @@
+ch = input()
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "Vowel")
+elif(ch=='!' or ch=='@' or ch=='#' or ch=='$' or ch=='%' or ch=='^' or ch=='&' or ch=='*' or ch=='(' or ch==')'):
+    print(ch, "invalid")
+else:    
+	print(ch, "Consonant")1
