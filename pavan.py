b4027f1a4b6741119cb4286a0ffbc0e8c83d44bc
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-05-07 19:18:11 +0530

    Update pavan.py

--- pavan.py
+++ pavan.py
@@ -2,8 +2,8 @@ ch = input()
 
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print(ch, "Vowel")
+    print("Vowel")
 elif(ch=='!' or ch=='@' or ch=='#' or ch=='$' or ch=='%' or ch=='^' or ch=='&' or ch=='*' or ch=='(' or ch==')'):
-    print(ch, "invalid")
+    print("invalid")
 else:    
-	print(ch, "Consonant")
+	print("Consonant")
